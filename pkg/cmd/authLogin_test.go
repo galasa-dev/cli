aa7e3575751751e1861f59b108ee8f8babceb9ab
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2023-07-31 11:49:52 +0100

    Add auth login unit test
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- pkg/cmd/authLogin_test.go
+++ pkg/cmd/authLogin_test.go
@@ -32,10 +32,9 @@ func NewAuthServletMock(t *testing.T, status int, mockResponse string) *httptest
 			assert.Contains(t, requestBodyStr, "access_token")
 
 			writer.Header().Set("Content-Type", "application/json")
+			writer.WriteHeader(status)
 			writer.Write([]byte(mockResponse))
 		}
-
-		writer.WriteHeader(status)
 	}))
 
 	return server
@@ -159,4 +158,37 @@ func TestLoginWithFailedFileWriteReturnsError(t *testing.T) {
 	// Should have created a folder for the parent package.
 	assert.NotNil(t, err, "Should return an error if writing the bearer token file fails")
 	assert.ErrorContains(t, err, "GAL1042E")
+}
+
+func TestLoginWithFailedTokenRequestReturnsError(t *testing.T) {
+	// Given...
+	mockFileSystem := files.NewMockFileSystem()
+	mockEnvironment := utils.NewMockEnv()
+	mockGalasaHome, _ := utils.NewGalasaHome(mockFileSystem, mockEnvironment, "")
+
+	galasactlYamlFilePath := mockGalasaHome.GetNativeFolderPath() + "/galasactl.yaml"
+
+	mockClientId := "dummyId"
+	mockSecret := "shhhh"
+	mockRefreshToken := "abcdefg"
+	mockFileSystem.WriteTextFile(galasactlYamlFilePath, fmt.Sprintf(
+		"apiVersion: v1\n"+
+		"auth:\n"+
+		"  client_id: %s\n"+
+		"  secret: %s\n"+
+		"  access_token: %s", mockClientId, mockSecret, mockRefreshToken))
+
+	mockResponse := `{"error":"something went wrong!"}`
+	server := NewAuthServletMock(t, 500, mockResponse)
+	defer server.Close()
+
+	apiServerUrl := server.URL
+
+	// When ...
+	err := Login(apiServerUrl, mockFileSystem, mockGalasaHome)
+
+	// Then...
+	// Should have created a folder for the parent package.
+	assert.NotNil(t, err, "Should return an error if the API request returns an error")
+	assert.ErrorContains(t, err, "GAL1091E")
 }
