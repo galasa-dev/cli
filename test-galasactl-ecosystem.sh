937a902c75ffd1990853085b3f585f192838ef6f
Holly Hunt <holly.hunt1101@hotmail.com>
2023-09-04 10:29:55 +0100

    retrying fixing the test ecosystem bug as it's breaking on tekton
    Signed-off-by: Holly Hunt <holly.hunt1101@hotmail.com>

--- test-galasactl-ecosystem.sh
+++ test-galasactl-ecosystem.sh
@@ -659,7 +659,7 @@ function runs_get_check_raw_format_output_with_older_to_than_from_age {
 
 #--------------------------------------------------------------------------
 function runs_get_check_requestor_parameter {
-    requestor=$USER
+    requestor=$(whoami)
     h2 "Performing runs get with details format providing a from age and requestor as $requestor..."
 
     cd ${BASEDIR}/temp
