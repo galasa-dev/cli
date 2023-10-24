83bab04f57dbd16b907f1041944939f532395a01
Holly Hunt <holly.hunt1101@hotmail.com>
2023-10-24 16:49:55 +0100

    created yaml formatter and util
    Signed-off-by: Holly Hunt <holly.hunt1101@hotmail.com>

---
+++ pkg/propertiesformatter/yamlFormatter.go
@@ -0,0 +1,53 @@
+/*
+ * Copyright contributors to the Galasa project
+ *
+ * SPDX-License-Identifier: EPL-2.0
+*/
+package propertiesformatter
+
+import (
+	"strings"
+	"github.com/galasa-dev/cli/pkg/utils"
+)
+
+// -----------------------------------------------------
+// Summary format.
+const (
+	YAML_FORMATTER_NAME = "summary"
+)
+
+type PropertyYamlFormatter struct {
+}
+
+func NewPropertyYamlFormatter() PropertyFormatter {
+	return new(PropertyYamlFormatter)
+}
+
+func (*PropertyYamlFormatter) GetName() string {
+	return YAML_FORMATTER_NAME
+}
+ 
+ func (*PropertyYamlFormatter) FormatProperties(cpsProperties []FormattableProperty) (string, error) {
+	var result string = ""
+	var err error = nil
+	buff := strings.Builder{}
+	totalProperties := len(cpsProperties)
+	counter := 0
+
+	buff.WriteString("apiVersion: galasa-dev/v1alpha1")
+	for _, property := range cpsProperties {
+		propertyString := ""
+
+		propertyString += utils.ConvertToYaml("GalasaProperty", property.Namespace, property.Name, property.Value)
+		if counter != totalProperties {
+			propertyString += "---\n"
+		}
+		counter ++
+		buff.WriteString(propertyString)
+	}
+
+
+ 
+	result = buff.String()
+	return result, err
+}
