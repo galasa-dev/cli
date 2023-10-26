157413d4460a216a04ce4d780528b234648cd740
Holly Hunt <holly.hunt1101@hotmail.com>
2023-10-26 14:53:17 +0100

    implemented propeprties formatters use galasaapi cps properties
    Signed-off-by: Holly Hunt <holly.hunt1101@hotmail.com>

---
+++ pkg/propertiesformatter/yamlFormatter_test.go
@@ -0,0 +1,84 @@
+/*
+* Copyright contributors to the Galasa project
+*
+* SPDX-License-Identifier: EPL-2.0
+ */
+package propertiesformatter
+
+import (
+	"testing"
+
+	"github.com/galasa-dev/cli/pkg/galasaapi"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestYamlFormatterNoDataReturnsBlankString(t *testing.T) {
+ 
+	formatter := NewPropertyYamlFormatter()
+	// No data to format...
+	formattableProperty := make([]galasaapi.CpsProperty, 0)
+ 
+	// When...
+	actualFormattedOutput, err := formatter.FormatProperties(formattableProperty)
+ 
+	assert.Nil(t, err)
+	expectedFormattedOutput := ""
+	assert.Equal(t, expectedFormattedOutput, actualFormattedOutput)
+}
+
+// func TestYamlFormatterSingleDataReturnsCorrectly(t *testing.T) {
+// 	// For..
+// 	formatter := NewPropertyYamlFormatter()
+// 	// No data to format...
+// 	formattableProperties := make([]FormattableProperty, 0)
+// 	formattableProperty1 := CreateFormattableTest("namespace", "name1", "value1")
+// 	formattableProperties = append(formattableProperties, formattableProperty1)
+ 
+// 	// When...
+// 	actualFormattedOutput, err := formatter.FormatProperties(formattableProperties)
+
+// 	// Then...
+// 	assert.Nil(t, err)
+// 	expectedFormattedOutput := `apiVersion: galasa-dev/v1alpha1
+// Kind: GalasaProperty
+// metadata:
+// 	cpsnamespace: namespace
+// 	name: name1
+// data:
+// 	value: value1
+// `
+// 	assert.Equal(t, expectedFormattedOutput, actualFormattedOutput)
+// }
+
+// func TestYamlFormatterMultipleDataSeperatesWithNewLine(t *testing.T) {
+// 	// For..
+// 	formatter := NewPropertyYamlFormatter()
+// 	// No data to format...
+// 	formattableProperties := make([]FormattableProperty, 0)
+// 	formattableProperty1 := CreateFormattableTest("namespace", "name1", "value1")
+// 	formattableProperties = append(formattableProperties, formattableProperty1)
+// 	formattableProperty2 := CreateFormattableTest("namespace", "name2", "value2")
+// 	formattableProperties = append(formattableProperties, formattableProperty2)
+ 
+// 	// When...
+// 	actualFormattedOutput, err := formatter.FormatProperties(formattableProperties)
+
+// 	// Then...
+// 	assert.Nil(t, err)
+// 	expectedFormattedOutput := `apiVersion: galasa-dev/v1alpha1
+// Kind: GalasaProperty
+// metadata:
+// 	cpsnamespace: namespace
+// 	name: name1
+// data:
+// 	value: value1
+// ---
+// Kind: GalasaProperty
+// metadata:
+// 	cpsnamespace: namespace
+// 	name: name2
+// data:
+// 	value: value2
+// `
+// 	assert.Equal(t, expectedFormattedOutput, actualFormattedOutput)
+// }
