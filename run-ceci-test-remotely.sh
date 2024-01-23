9edccc2b60874b813aa92d5a87128d4c3bc13fe3
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-01-23 16:16:17 +0000

    merged eamon's code into this branch
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

---
+++ run-ceci-test-remotely.sh
@@ -0,0 +1,69 @@
+#! /usr/bin/env bash 
+
+#
+# Copyright contributors to the Galasa project
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Objectives: Sets the version number of the galasa-boot.
+# 
+#-----------------------------------------------------------------------------------------                   
+
+# Where is this script executing from ?
+BASEDIR=$(dirname "$0");pushd $BASEDIR 2>&1 >> /dev/null ;BASEDIR=$(pwd);popd 2>&1 >> /dev/null
+# echo "Running from directory ${BASEDIR}"
+export ORIGINAL_DIR=$(pwd)
+# cd "${BASEDIR}"
+
+cd "${BASEDIR}/.."
+WORKSPACE_DIR=$(pwd)
+
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Set Colors
+#
+#-----------------------------------------------------------------------------------------                   
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+white=$(tput setaf 7)
+tan=$(tput setaf 202)
+blue=$(tput setaf 25)
+
+#-----------------------------------------------------------------------------------------                   
+#
+# Headers and Logging
+#
+#-----------------------------------------------------------------------------------------                   
+underline() { printf "${underline}${bold}%s${reset}\n" "$@" ;}
+h1() { printf "\n${underline}${bold}${blue}%s${reset}\n" "$@" ;}
+h2() { printf "\n${underline}${bold}${white}%s${reset}\n" "$@" ;}
+debug() { printf "${white}%s${reset}\n" "$@" ;}
+info() { printf "${white}➜ %s${reset}\n" "$@" ;}
+success() { printf "${green}✔ %s${reset}\n" "$@" ;}
+error() { printf "${red}✖ %s${reset}\n" "$@" ;}
+warn() { printf "${tan}➜ %s${reset}\n" "$@" ;}
+bold() { printf "${bold}%s${reset}\n" "$@" ;}
+note() { printf "\n${underline}${bold}${blue}Note:${reset} ${blue}%s${reset}\n" "$@" ;}
+
+
+cd temp
+
+${BASEDIR}/bin/galasactl-darwin-arm64 runs prepare --portfolio test.yaml \
+--bootstrap $GALASA_BOOTSTRAP \
+--stream inttests \
+--test \
+local.CECILocalJava11Ubuntu \
+--log -
+
+${BASEDIR}/bin/galasactl-darwin-arm64 runs submit --bootstrap $GALASA_BOOTSTRAP \
+--portfolio test.yaml \
+--reportyaml tests.yaml \
+--overridefile - \
+--log -
