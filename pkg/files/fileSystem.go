95ced9c6a69da805b35333e2ab09903b58919789
Eamonn Mansour <47121388+eamansour@users.noreply.github.com>
2024-01-24 13:57:54 +0000

    Close artifact file handles after writing them to the filesystem
    Signed-off-by: Eamonn Mansour <47121388+eamansour@users.noreply.github.com>

--- pkg/files/fileSystem.go
+++ pkg/files/fileSystem.go
@@ -31,7 +31,7 @@ type FileSystem interface {
 	DeleteFile(path string)
 
 	// Creates a file in the file system if it can.
-	Create(path string) (io.Writer, error)
+	Create(path string) (io.WriteCloser, error)
 
 	// Returns the normal extension used for executable files.
 	// ie: The .exe suffix in windows, or "" in unix-like systems.
@@ -73,7 +73,7 @@ func NewOSFileSystem() FileSystem {
 // Interface methods...
 // ------------------------------------------------------------------------------------
 
-func (osFS *OSFileSystem) Create(path string) (io.Writer, error) {
+func (osFS *OSFileSystem) Create(path string) (io.WriteCloser, error) {
 	fileWriter, err := os.Create(path)
 	return fileWriter, err
 }
