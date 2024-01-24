9baab02952bb84c18fe577a8edf2694bc7c0acc3
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-01-24 16:52:50 +0000

    extra unit testing of image rendering with image capturing
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- pkg/images/imageExpander_test.go
+++ pkg/images/imageExpander_test.go
@@ -6,6 +6,8 @@
 package images
 
 import (
+	"log"
+	"os"
 	"testing"
 
 	"github.com/galasa-dev/cli/pkg/files"
@@ -48,64 +50,94 @@ func TestCalculatesBlankPathIfPathDoesntContainTerminals(t *testing.T) {
 	}
 }
 
-// func TestCanExpandAGzFileToAnImageFile(t *testing.T) {
+func TestCanExpandAGzFileToAnImageFile(t *testing.T) {
 
-// 	realFs := files.NewOSFileSystem()
+	realFs := files.NewOSFileSystem()
 
-// 	var gzContents []byte
-// 	var err error
+	var gzContents []byte
+	var err error
 
-// 	gzContents, err = realFs.ReadBinaryFile("./testdata/term1-00001.gz")
+	gzContents, err = realFs.ReadBinaryFile("./testdata/gzipExample/term1-00001.gz")
 
-// 	assert.Nil(t, err, "could not load the real gz file data")
-// 	if err == nil {
+	assert.Nil(t, err, "could not load the real gz file data")
+	if err == nil {
 
-// 		// Load the real gz contents into the mock file system...
-// 		// so any image files we create don't infect the real file system.
-// 		fs := files.NewMockFileSystem()
-// 		err = fs.WriteBinaryFile("/U423/zos3270/terminals/term1/term1-0001.gz", gzContents)
-// 		assert.Nil(t, err, "could not write real gz contents into the mock file system")
-// 		if err == nil {
+		// Load the real gz contents into the mock file system...
+		// so any image files we create don't infect the real file system.
+		fs := files.NewMockFileSystem()
+		err = fs.WriteBinaryFile("/U423/zos3270/terminals/term1/term1-1.gz", gzContents)
+		assert.Nil(t, err, "could not write real gz contents into the mock file system")
+		if err == nil {
 
-// 			renderer := NewImageRenderer()
-// 			expander := NewImageExpander(fs, renderer)
+			renderer := NewImageRenderer()
+			expander := NewImageExpander(fs, renderer)
 
-// 			// When...
-// 			err = expander.ExpandImages("/U423")
-// 			assert.Nil(t, err, "could not expand images")
-// 			if err == nil {
+			// When...
+			err = expander.ExpandImages("/U423")
+			assert.Nil(t, err, "could not expand images")
+			if err == nil {
 
-// 				// Then...
-// 				var isExists bool
-// 				isExists, err = fs.DirExists("/U423/zos3270/images/term1")
-// 				assert.Nil(t, err, "could not find out if file exists or not")
-// 				if err == nil {
+				// Then...
+				var isExists bool
+				isExists, err = fs.DirExists("/U423/zos3270/images/term1")
+				assert.Nil(t, err, "could not find out if file exists or not")
+				if err == nil {
 
-// 					assert.True(t, isExists, "Image folder %s was not created.", "/U423/zos3270/images/term1")
-// 					if isExists {
+					assert.True(t, isExists, "Image folder %s was not created.", "/U423/zos3270/images/term1")
+					if isExists {
 
-// 						// Read the rendered file contents.
-// 						var renderedContents []byte
-// 						renderedContents, err = fs.ReadBinaryFile("/U423/zos3270/images/term1/term1-0001.png")
-// 						assert.Nil(t, err, "could not read rendered file")
-// 						if err == nil {
+						// Read the rendered file contents.
+						var renderedContents []byte
+						renderedContents, err = fs.ReadBinaryFile("/U423/zos3270/images/term1/term1-00001.png")
+						assert.Nil(t, err, "could not read rendered file")
+						if err == nil {
 
-// 							// Read the file contents which we think the image should be once rendered.
-// 							var expectedContents []byte
-// 							expectedContents, err = realFs.ReadBinaryFile("./testdata/term1-00001.png")
-// 							assert.Nil(t, err, "could not read image file we will compare against")
-// 							if err == nil {
+							// Read the file contents which we think the image should be once rendered.
+							var expectedContents []byte
+							expectedContents, err = realFs.ReadBinaryFile("./testdata/gzipExample/term1-00001.png")
+							assert.Nil(t, err, "could not read image file we will compare against")
+							if err == nil {
 
-// 								// Compare the files.
-// 								if assert.Equal(t, expectedContents, renderedContents, "Rendered file does not match the expected") {
+								// Compare the files.
+								var isSame bool = true
+								renderedImageLength := len(renderedContents)
+								expectedImageLength := len(expectedContents)
+								if renderedImageLength != expectedImageLength {
+									assert.Fail(t, "error", "rendered contents length %v is different to the expected contents %v", renderedImageLength, expectedImageLength)
+									isSame = false
+								} else {
 
-// 									assert.Equal(t, 1, expander.GetExpandedImageFileCount(), "wrong number of expanded files counted.")
-// 								}
-// 							}
-// 						}
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// }
+									for i, valueGotBack := range renderedContents {
+										valueExpected := expectedContents[i]
+
+										if valueGotBack != valueExpected {
+											isSame = false
+											assert.Fail(t, "error", "rendered image byte %d differs from expected image byte %d", i, i)
+											break
+										}
+
+									}
+								}
+
+								if !isSame {
+									// Files don't match, so save the file we got for manual inspection.
+									// If the user wants, they can copy this file into the project as expected test data.
+									var renderedFile *os.File
+
+									renderedFile, err = os.CreateTemp("", "rendered-image-*.png")
+									defer renderedFile.Close()
+									renderedFile.Write(renderedContents)
+
+									log.Printf("A copy of the rendered file has been saved to %s for manual inspection if required.\n", renderedFile.Name())
+								} else {
+									// The example gz file contains 10 screens, each of which should be rendered
+									assert.Equal(t, 10, expander.GetExpandedImageFileCount(), "wrong number of expanded files counted.")
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
