637ef63353bb5a25dec0419074d0bb4abd5b1669
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-01-26 11:14:59 +0000

    gathered beans into single file
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- pkg/images/imageFileWriter.go
+++ pkg/images/imageFileWriter.go
@@ -14,6 +14,7 @@ import (
 type ImageFileWriter interface {
 	WriteImageFile(simpleFileName string, imageBytes []byte) error
 	GetImageFilesWrittenCount() int
+	IsImageFileWritable(simpleFileName string) (bool, error)
 }
 
 type ImageFileWriterImpl struct {
@@ -34,21 +35,45 @@ func (writer *ImageFileWriterImpl) GetImageFilesWrittenCount() int {
 	return writer.imageFilesWrittenCount
 }
 
+func (writer *ImageFileWriterImpl) IsImageFileWritable(simpleFileName string) (bool, error) {
+	fullyQualifiedTargetImageFilePath := writer.simpleFileToFullyQualifiedFilePath(simpleFileName)
+	isWritable, err := writer.isFullyQualifiedImageFileWritable(fullyQualifiedTargetImageFilePath)
+	return isWritable, err
+}
+
+func (writer *ImageFileWriterImpl) simpleFileToFullyQualifiedFilePath(simpleFileName string) string {
+	return writer.imageFolderPath + writer.fs.GetFilePathSeparator() + simpleFileName
+}
+
+func (writer *ImageFileWriterImpl) isFullyQualifiedImageFileWritable(qualifiedFileName string) (bool, error) {
+	var isExistsAlready bool
+	var err error
+	var isWritable bool = false
+
+	isExistsAlready, err = writer.fs.Exists(qualifiedFileName)
+	if err == nil {
+		if isExistsAlready {
+			log.Printf("File %s already exists. So not over-writing it.\n", qualifiedFileName)
+		} else {
+			// It's writeable.
+			isWritable = true
+		}
+	}
+	return isWritable, err
+}
+
 func (writer *ImageFileWriterImpl) WriteImageFile(simpleFileName string, imageBytes []byte) error {
 	var err error
-	targetImageFilePath := writer.imageFolderPath + writer.fs.GetFilePathSeparator() + simpleFileName
+	fullyQualifiedTargetImageFilePath := writer.simpleFileToFullyQualifiedFilePath(simpleFileName)
+
+	var isWritable bool
+	isWritable, err = writer.isFullyQualifiedImageFileWritable(fullyQualifiedTargetImageFilePath)
 	if err == nil {
-		var isExistsAlready bool
-		isExistsAlready, err = writer.fs.Exists(targetImageFilePath)
-		if err == nil {
-			if isExistsAlready {
-				log.Printf("File %s already exists. So not over-writing it.\n", targetImageFilePath)
-			} else {
-				err = writer.fs.WriteBinaryFile(targetImageFilePath, imageBytes)
-				if err == nil {
-					writer.imageFilesWrittenCount = writer.imageFilesWrittenCount + 1
-					log.Printf("Image file has been created: %s\n", targetImageFilePath)
-				}
+		if isWritable {
+			err = writer.fs.WriteBinaryFile(fullyQualifiedTargetImageFilePath, imageBytes)
+			if err == nil {
+				writer.imageFilesWrittenCount = writer.imageFilesWrittenCount + 1
+				log.Printf("Image file has been created: %s\n", fullyQualifiedTargetImageFilePath)
 			}
 		}
 	}
--- pkg/images/imageRenderer.go
+++ pkg/images/imageRenderer.go
@@ -22,28 +22,28 @@ import (
 )
 
 var (
-    FONT_WIDTH = 7
-    FONT_HEIGHT = 13
+	FONT_WIDTH  = 7
+	FONT_HEIGHT = 13
 
-    DEFAULT_COLOR = color.RGBA{0, 255, 0, 255}
-    NEUTRAL       = color.RGBA{255, 255, 255, 255}
-    RED           = color.RGBA{255, 0, 0, 255}
-    GREEN         = color.RGBA{0, 255, 0, 255}
-    BLUE          = color.RGBA{0, 0, 255, 255}
-    PINK          = color.RGBA{255, 0, 204, 255}
-    TURQUOISE     = color.RGBA{64, 224, 208, 255}
-    YELLOW        = color.RGBA{255, 255, 0, 255}
+	DEFAULT_COLOR = color.RGBA{0, 255, 0, 255}
+	NEUTRAL       = color.RGBA{255, 255, 255, 255}
+	RED           = color.RGBA{255, 0, 0, 255}
+	GREEN         = color.RGBA{0, 255, 0, 255}
+	BLUE          = color.RGBA{0, 0, 255, 255}
+	PINK          = color.RGBA{255, 0, 204, 255}
+	TURQUOISE     = color.RGBA{64, 224, 208, 255}
+	YELLOW        = color.RGBA{255, 255, 0, 255}
 
-    colors = map[string]color.RGBA{
-        "d": DEFAULT_COLOR,
-        "r": RED,
-        "g": GREEN,
-        "b": BLUE,
-        "p": PINK,
-        "t": TURQUOISE,
-        "y": YELLOW,
-        "n": NEUTRAL,
-    }
+	colors = map[string]color.RGBA{
+		"d": DEFAULT_COLOR,
+		"r": RED,
+		"g": GREEN,
+		"b": BLUE,
+		"p": PINK,
+		"t": TURQUOISE,
+		"y": YELLOW,
+		"n": NEUTRAL,
+	}
 )
 
 type ImageRenderer interface {
@@ -65,13 +65,24 @@ func (renderer *ImageRendererImpl) RenderJsonBytesToImageFiles(jsonBinary []byte
 	terminal, err = convertJsonBytesToTerminal(jsonBinary)
 	if err == nil {
 		for _, terminalImage := range terminal.Images {
-			image := renderTerminalImage(terminalImage)
 
-			var pngImageBytes []byte
-			pngImageBytes, err = encodeImageToPng(image)
+			pngFileName := fmt.Sprintf("%s-%05d.png", terminal.Id, terminalImage.Sequence)
+
+			var isWritable bool
+			isWritable, err = writer.IsImageFileWritable(pngFileName)
 			if err == nil {
-				pngFileName := fmt.Sprintf("%s-%05d.png", terminal.Id, terminalImage.Sequence)
-				err = writer.WriteImageFile(pngFileName, pngImageBytes)
+				// Only render the image if we will be able to write it out later.
+				// ie: Don't do all the work if the file already exists.
+				// When the same root folder is scanned for a second time, we want to minimise the work done
+				if isWritable {
+					image := renderTerminalImage(terminalImage)
+
+					var pngImageBytes []byte
+					pngImageBytes, err = encodeImageToPng(image)
+					if err == nil {
+						err = writer.WriteImageFile(pngFileName, pngImageBytes)
+					}
+				}
 			}
 		}
 	}
@@ -83,49 +94,63 @@ func convertJsonBytesToTerminal(terminalJsonBytes []byte) (Terminal, error) {
 	var terminal Terminal
 	var err error = nil
 
-    err = json.Unmarshal(terminalJsonBytes, &terminal)
-    if err != nil {
-        err = galasaErrors.NewGalasaError(galasaErrors.GALASA_ERROR_BAD_TERMINAL_JSON_FORMAT, err.Error())
-    }
+	err = json.Unmarshal(terminalJsonBytes, &terminal)
+	if err != nil {
+		err = galasaErrors.NewGalasaError(galasaErrors.GALASA_ERROR_BAD_TERMINAL_JSON_FORMAT, err.Error())
+	}
 	return terminal, err
 }
 
 // Renders an RGBA image representation of a 3270 terminal and returns the rendered image
 func renderTerminalImage(terminalImage TerminalImage) *image.RGBA {
-    targetColumnCount := terminalImage.ImageSize.Columns
-    targetRowCount := terminalImage.ImageSize.Rows + 3
+	targetColumnCount := terminalImage.ImageSize.Columns
+	targetRowCount := terminalImage.ImageSize.Rows + 3
 
-    imagePixelWidth := targetColumnCount * FONT_WIDTH
-    imagePixelHeight := targetRowCount * FONT_HEIGHT
-    img := createImageBase(imagePixelWidth, imagePixelHeight)
-    context := createImageDrawer(img)
+	imagePixelWidth := targetColumnCount * FONT_WIDTH
+	imagePixelHeight := targetRowCount * FONT_HEIGHT
+	img := createImageBase(imagePixelWidth, imagePixelHeight)
+	context := createImageDrawer(img)
 
-    for _, field := range terminalImage.Fields {
-        column := field.Column
-        row := field.Row
+	for _, field := range terminalImage.Fields {
+		column := field.Column
+		row := field.Row
 
-        for _, contents := range field.Contents {
-            // Field contents can sometimes span multiple rows, so draw each character individually,
-            // adjusting the current row whenever the image column boundary is reached
-            for _, char := range contents.getCharacters() {
-                if column >= targetColumnCount {
-                    column = 0
-                    row++
-                }
-                textColor := getColor(field.ForegroundColor)
-                drawString(context, column, row, string(char), textColor)
-                column++
-            }
-        }
-    }
-    statusText := getStatusText(terminalImage, terminalImage.ImageSize.Columns, terminalImage.ImageSize.Rows)
-    drawString(context, 0, targetRowCount - 2, statusText, DEFAULT_COLOR)
-    return img
+		for _, contents := range field.Contents {
+			// Field contents can sometimes span multiple rows, so draw each character individually,
+			// adjusting the current row whenever the image column boundary is reached
+			for _, char := range getCharacters(&contents) {
+				if column >= targetColumnCount {
+					column = 0
+					row++
+				}
+				textColor := getColor(field.ForegroundColor)
+				drawString(context, column, row, string(char), textColor)
+				column++
+			}
+		}
+	}
+	statusText := getStatusText(terminalImage, terminalImage.ImageSize.Columns, terminalImage.ImageSize.Rows)
+	drawString(context, 0, targetRowCount-2, statusText, DEFAULT_COLOR)
+	return img
+}
+
+func getCharacters(fieldContents *FieldContents) []rune {
+	var contents []rune
+	if fieldContents.Characters != nil {
+		// If the terminal JSON defines the contents of a field in characters,
+		// then convert the character strings into runes
+		for _, char := range fieldContents.Characters {
+			contents = append(contents, []rune(char)...)
+		}
+	} else {
+		contents = []rune(fieldContents.Text)
+	}
+	return contents
 }
 
 // Encodes a rendered 3270 terminal image in PNG format and returns the resulting byte array
 func encodeImageToPng(image *image.RGBA) ([]byte, error) {
-    var err error = nil
+	var err error = nil
 
 	buf := new(bytes.Buffer)
 	err = png.Encode(buf, image)
@@ -133,66 +158,66 @@ func encodeImageToPng(image *image.RGBA) ([]byte, error) {
 		err = galasaErrors.NewGalasaError(galasaErrors.GALASA_ERROR_PNG_ENCODING_FAILED, err.Error())
 	}
 
-    return buf.Bytes(), err
+	return buf.Bytes(), err
 }
 
 // Creates a black rectangle image with the given dimensions in pixels
 func createImageBase(imagePixelWidth int, imagePixelHeight int) *image.RGBA {
-    rect := image.Rect(0, 0, imagePixelWidth, imagePixelHeight)
-    img := image.NewRGBA(rect)
+	rect := image.Rect(0, 0, imagePixelWidth, imagePixelHeight)
+	img := image.NewRGBA(rect)
 
-    // Draw a black background onto the image
-    draw.Draw(img, img.Bounds(), image.Black, image.Pt(0, 0), draw.Src)
-    return img
+	// Draw a black background onto the image
+	draw.Draw(img, img.Bounds(), image.Black, image.Pt(0, 0), draw.Src)
+	return img
 }
 
 // Creates a drawer instance to draw text on a given image
 func createImageDrawer(img *image.RGBA) *font.Drawer {
-    drawer := &font.Drawer{
-        Dst:  img,
-        Face: initFontFaces(),
-    }
+	drawer := &font.Drawer{
+		Dst:  img,
+		Face: initFontFaces(),
+	}
 
-    return drawer
+	return drawer
 }
 
 // Draws a string of text onto an image at the given column and row (x, y) coordinates
 func drawString(drawer *font.Drawer, column int, row int, text string, textColor color.RGBA) {
-    startPoint := fixed.Point26_6{ X: fixed.I(column * FONT_WIDTH), Y: fixed.I((row + 1) * FONT_HEIGHT)}
+	startPoint := fixed.Point26_6{X: fixed.I(column * FONT_WIDTH), Y: fixed.I((row + 1) * FONT_HEIGHT)}
 
-    drawer.Src = image.NewUniform(textColor)
-    drawer.Dot = startPoint
+	drawer.Src = image.NewUniform(textColor)
+	drawer.Dot = startPoint
 
-    drawer.DrawString(text)
+	drawer.DrawString(text)
 }
 
 // Returns a string containing the content of the status row to be displayed at the bottom
 // of a 3270 image
 func getStatusText(terminalImage TerminalImage, columns int, rows int) string {
-    var buff strings.Builder
+	var buff strings.Builder
 
-    buff.WriteString(terminalImage.Id)
-    buff.WriteString(" - ")
+	buff.WriteString(terminalImage.Id)
+	buff.WriteString(" - ")
 
-    buff.WriteString(strconv.Itoa(columns))
-    buff.WriteString("x")
-    buff.WriteString(strconv.Itoa(rows))
-    buff.WriteString(" - ")
+	buff.WriteString(strconv.Itoa(columns))
+	buff.WriteString("x")
+	buff.WriteString(strconv.Itoa(rows))
+	buff.WriteString(" - ")
 
-    if terminalImage.Inbound {
-        buff.WriteString("Inbound ")
-    } else {
-        buff.WriteString("Outbound - ")
-        buff.WriteString(terminalImage.Aid)
-    }
-    return buff.String()
+	if terminalImage.Inbound {
+		buff.WriteString("Inbound ")
+	} else {
+		buff.WriteString("Outbound - ")
+		buff.WriteString(terminalImage.Aid)
+	}
+	return buff.String()
 }
 
 // Returns a color from the colors map that matches the given single-character identifier
 func getColor(colorIdentifier string) color.RGBA {
-    color := DEFAULT_COLOR
-    if matchedColor, ok := colors[colorIdentifier]; ok {
-        color = matchedColor
-    }
-    return color
+	color := DEFAULT_COLOR
+	if matchedColor, ok := colors[colorIdentifier]; ok {
+		color = matchedColor
+	}
+	return color
 }
