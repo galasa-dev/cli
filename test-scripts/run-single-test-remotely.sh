073e6615cc3a19901c67aa2923bffa794688e4c4
Mike Cobbett <77053+techcobweb@users.noreply.github.com>
2024-10-24 15:02:28 +0100

    Added a better verision of the test script
    Signed-off-by: Mike Cobbett <77053+techcobweb@users.noreply.github.com>

--- test-scripts/run-single-test-remotely.sh
+++ test-scripts/run-single-test-remotely.sh
@@ -9,20 +9,13 @@
 # Where is this script executing from ?
 BASEDIR=$(dirname "$0");pushd $BASEDIR 2>&1 >> /dev/null ;BASEDIR=$(pwd);popd 2>&1 >> /dev/null
 export ORIGINAL_DIR=$(pwd)
-cd "${BASEDIR}"
 
-# export GALASA_HOME=${BASEDIR}/../temp/home
 
-# galasactl runs submit \
-# --class dev.galasa.inttests/dev.galasa.inttests.core.local.CoreLocalJava11Ubuntu \
-# --stream inttests \
-# --throttle 1 \
-# --poll 10 \
-# --progress 1 \
-# --noexitcodeontestfailures \
-# --log - \
-# --overridefile /Users/mcobbett/builds/galasa/code/src/github.com/galasa-dev/cli/temp/home/overrides.properties
+mkdir -p ${BASEDIR}/../temp/home
+cd ${BASEDIR}/../temp/home
+export GALASA_HOME=$(pwd)
 
+cd "${BASEDIR}/.."
 
 galasactl runs submit \
 --class dev.galasa.inttests/dev.galasa.inttests.core.local.CoreLocalJava11Ubuntu \
@@ -32,7 +25,7 @@ galasactl runs submit \
 --progress 1 \
 --noexitcodeontestfailures \
 --log - \
---overridefile /Users/mcobbett/builds/galasa/code/src/github.com/galasa-dev/cli/temp/home/overrides.properties
+--overridefile ${GALASA_HOME}/overrides.properties
 
 # galasactl runs prepare --portfolio my.portfolio --class dev.galasa.inttests/dev.galasa.inttests.core.local.CoreLocalJava11Ubuntu --stream inttests
 # galasactl runs submit --portfolio my.portfolio --log -
