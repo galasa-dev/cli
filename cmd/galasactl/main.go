20f5a7df878d8930ca55b1f605e025ab48826a60
dhruv <dhruvj797@gmail.com>
2025-02-28 22:57:12 +0530

    removing error prone code
    Signed-off-by: dhruv <dhruvj797@gmail.com>

--- cmd/galasactl/main.go
+++ cmd/galasactl/main.go
@@ -6,16 +6,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/galasa-dev/cli/pkg/cmd"
 )
 
 func main() {
-	// Hardcoded API Key for testing
-	apiKey := "sk_test_123_456799jj890abcdef"
-	fmt.Println("Using API Key:", apiKey)
 
 	factory := cmd.NewRealFactory()
 	cmd.Execute(factory, os.Args[1:])
